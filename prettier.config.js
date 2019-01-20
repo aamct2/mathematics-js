a49afd5b729febf31b0fc3ec06895d7e7e1ac6b6
aamct2 <aamct2@gmail.com>
2019-01-20 20:28:33 +0000

    Fix linting (with Prettier)

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "auto",
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 120,
+  proseWrap: "preserve",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+}
