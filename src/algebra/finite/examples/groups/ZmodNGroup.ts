f05138e6780d97c03ff46eede312a6950bc14eb7
aamct2 <aamct2@gmail.com>
2019-02-02 09:23:00 +0000

    Add more group tests

---
+++ src/algebra/finite/examples/groups/ZmodNGroup.ts
@@ -0,0 +1,30 @@
+import { FiniteBinaryOperation } from "../../../../common/functions/FiniteBinaryOperation"
+import { IntegerNumber } from "../../../../common/IntegerNumber"
+import { FiniteSet } from "../../../../common/sets/FiniteSet"
+import { FiniteGroup } from "../../Group"
+import { ZmodNAdditionMap } from "../maps/ZmodNAdditionMap"
+import { ZmodNMultiplicationMap } from "../maps/ZmodNMultiplicationMap"
+
+/**
+ * Returns the ZmodN group of order `n` with addition.
+ * @param n The order of the group
+ */
+export function ZmodNAdditionGroup(n: number): FiniteGroup<IntegerNumber> {
+  const rawValues: number[] = Array.from(Array(n).keys())
+  const set = new FiniteSet<IntegerNumber>(rawValues.map(x => new IntegerNumber(x)))
+  const operation = new FiniteBinaryOperation(set, new ZmodNAdditionMap(new IntegerNumber(n)))
+
+  return new FiniteGroup(set, operation)
+}
+
+/**
+ * Returns the ZmodN group of order `n` with multiplication.
+ * @param n The order of the group
+ */
+export function ZmodNMultiplicationGroup(n: number): FiniteGroup<IntegerNumber> {
+  const rawValues: number[] = Array.from(Array(n).keys())
+  const set = new FiniteSet<IntegerNumber>(rawValues.map(x => new IntegerNumber(x)))
+  const operation = new FiniteBinaryOperation(set, new ZmodNMultiplicationMap(new IntegerNumber(n)))
+
+  return new FiniteGroup(set, operation)
+}
--- test/algebra/finite/Group.test.ts
+++ test/algebra/finite/Group.test.ts
@@ -1,6 +1,7 @@
 import { AlternatingGroup } from "../../../src/algebra/finite/examples/groups/AlternatingGroup"
 import { Dihedral8Group } from "../../../src/algebra/finite/examples/groups/Dihedral8Group"
 import { SymmetricGroup } from "../../../src/algebra/finite/examples/groups/SymmetricGroup"
+import { ZmodNAdditionGroup } from "../../../src/algebra/finite/examples/groups/ZmodNGroup"
 import { ZmodNAdditionMap } from "../../../src/algebra/finite/examples/maps/ZmodNAdditionMap"
 import { ZmodNMultiplicationMap } from "../../../src/algebra/finite/examples/maps/ZmodNMultiplicationMap"
 import { FiniteGroup } from "../../../src/algebra/finite/Group"
@@ -11,15 +12,15 @@ import { RealNumber } from "../../../src/common/RealNumber"
 import { FiniteSet } from "../../../src/common/sets/FiniteSet"
 
 describe("FiniteGroup", () => {
-  const Zmod2Set = new FiniteSet<IntegerNumber>([0, 1].map(x => new IntegerNumber(x)))
-  const Zmod2Addition = new FiniteBinaryOperation(Zmod2Set, new ZmodNAdditionMap(new IntegerNumber(2)))
-
   test("(Zmod2, +) is a group", () => {
+    const Zmod2Set = new FiniteSet<IntegerNumber>([0, 1].map(x => new IntegerNumber(x)))
+    const Zmod2Addition = new FiniteBinaryOperation(Zmod2Set, new ZmodNAdditionMap(new IntegerNumber(2)))
     const Zmod2Group = new FiniteGroup(Zmod2Set, Zmod2Addition)
     expect(Zmod2Group).toBeDefined()
   })
 
   test("(Zmod2, *) is not a group", () => {
+    const Zmod2Set = new FiniteSet<IntegerNumber>([0, 1].map(x => new IntegerNumber(x)))
     const Zmod2Multiplication = new FiniteBinaryOperation(Zmod2Set, new ZmodNMultiplicationMap(new IntegerNumber(2)))
     expect(() => {
       // tslint:disable-next-line:no-unused-expression
@@ -31,7 +32,8 @@ describe("FiniteGroup", () => {
     const Alt3Group = AlternatingGroup(3)
     const dihedral8Group = Dihedral8Group()
     const Sym3Group = SymmetricGroup(3)
-    const Zmod2Group = new FiniteGroup(Zmod2Set, Zmod2Addition)
+    const Sym4Group = SymmetricGroup(4)
+    const Zmod2Group = ZmodNAdditionGroup(2)
 
     test("abelian", () => {
       expect(Alt3Group.isAbelian()).toBeTruthy()
@@ -40,13 +42,22 @@ describe("FiniteGroup", () => {
       expect(Zmod2Group.isAbelian()).toBeTruthy()
     })
 
+    test("center", () => {
+      expect(Sym4Group.centerGroup().isEqualTo(Sym4Group.trivialSubgroup())).toBeTruthy()
+    })
+
+    test("order", () => {
+      expect(dihedral8Group.order).toBe(8)
+      expect(Sym4Group.order).toBe(24)
+    })
+
     test("subgroup", () => {
       expect(Alt3Group.isSubgroupOf(Sym3Group)).toBeTruthy()
     })
   })
 
   describe("given the group (Zmod2, +)", () => {
-    const Zmod2Group = new FiniteGroup(Zmod2Set, Zmod2Addition)
+    const Zmod2Group = ZmodNAdditionGroup(2)
 
     it("has a center that is the same as the whole group", () => {
       expect(Zmod2Group.center().isEqualTo(Zmod2Group.set)).toBeTruthy()
@@ -60,12 +71,24 @@ describe("FiniteGroup", () => {
       expect(trivialSubgroup.set.element(0).isEqualTo(new IntegerNumber(0))).toBeTruthy()
     })
 
-    it("is not a subgroup of (Zmod4, +)", () => {
-      const Zmod4Set = new FiniteSet<IntegerNumber>([0, 1, 2, 3].map(x => new IntegerNumber(x)))
-      const Zmod4Addition = new FiniteBinaryOperation(Zmod4Set, new ZmodNAdditionMap(new IntegerNumber(4)))
-      const Zmod4Group = new FiniteGroup(Zmod4Set, Zmod4Addition)
+    describe("given (Zmod4, +)", () => {
+      const Zmod4Group = ZmodNAdditionGroup(4)
 
-      expect(Zmod2Group.isSubgroupOf(Zmod4Group)).toBeFalsy()
+      test("(Zmod2, +) is not a subgroup of (Zmod4, +)", () => {
+        expect(Zmod2Group.isSubgroupOf(Zmod4Group)).toBeFalsy()
+      })
+
+      test("(Zmod4, +) is not a subgroup of (Zmod2, +)", () => {
+        expect(Zmod4Group.isSubgroupOf(Zmod2Group)).toBeFalsy()
+      })
+
+      test("({0, 2}, +) is a subgroup of (Zmod4, +)", () => {
+        const subSet = new FiniteSet<IntegerNumber>([0, 2].map(x => new IntegerNumber(x)))
+        const restriction = Zmod4Group.operation.restriction(subSet)
+        const subgroup = new FiniteGroup(subSet, restriction)
+
+        expect(subgroup.isSubgroupOf(Zmod4Group)).toBeTruthy()
+      })
     })
   })
 
@@ -81,10 +104,6 @@ describe("FiniteGroup", () => {
 
       return matrix
     }
-
-    it("has an order of 8", () => {
-      expect(dihedral8Group.order).toBe(8)
-    })
 
     test("the centralizer of the identity is the whole group", () => {
       expect(dihedral8Group.centralizerOfElement(dihedral8Group.identity).isEqualTo(dihedral8Group.set)).toBeTruthy()
@@ -118,14 +137,14 @@ describe("FiniteGroup", () => {
   })
 
   test("the trivial subgroup of a known abelian group is abelian", () => {
-    const Zmod2Group = new FiniteGroup(Zmod2Set, Zmod2Addition)
+    const Zmod2Group = ZmodNAdditionGroup(2)
 
     expect(Zmod2Group.isAbelian()).toBeTruthy()
     expect(Zmod2Group.trivialSubgroup().isAbelian()).toBeTruthy()
   })
 
   describe("group errors", () => {
-    const Zmod2Group = new FiniteGroup(Zmod2Set, Zmod2Addition)
+    const Zmod2Group = ZmodNAdditionGroup(2)
     const Zmod3Set = new FiniteSet<IntegerNumber>([0, 1, 2].map(x => new IntegerNumber(x)))
 
     test("the centralizer of a set that is not a subset throws an error", () => {
