89b496539367d0f5a5c252504041660cd7e003d8
aamct2 <aamct2@gmail.com>
2019-02-04 18:58:53 +0000

    Remove ZmodNMultiplicationGroup
    as ZmodN does not normally form a group under multiplication

--- src/algebra/finite/examples/groups/ZmodNGroup.ts
+++ src/algebra/finite/examples/groups/ZmodNGroup.ts
@@ -15,15 +15,3 @@ export function ZmodNAdditionGroup(n: number): FiniteGroup<IntegerNumber> {
 
   return new FiniteGroup(set, operation)
 }
-
-/**
- * Returns the ZmodN group of order `n` with multiplication.
- * @param n The order of the group
- */
-export function ZmodNMultiplicationGroup(n: number): FiniteGroup<IntegerNumber> {
-  const rawValues: number[] = Array.from(Array(n).keys())
-  const set = new FiniteSet<IntegerNumber>(rawValues.map(x => new IntegerNumber(x)))
-  const operation = new FiniteBinaryOperation(set, new ZmodNMultiplicationMap(new IntegerNumber(n)))
-
-  return new FiniteGroup(set, operation)
-}
